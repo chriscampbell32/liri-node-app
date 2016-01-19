c690a7ecaa68aed807e2440958d1d90512a859c2
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-19 14:24:03 -0500

    twitter function is working properly tyty

--- liri.js
+++ liri.js
@@ -1,5 +1,34 @@
-var twitterKeys = require("./keys.js");
-var spotifySong = require('spotify');
+var keys = require("./keys.js");
+var Twitter = require('twitter');
+var spotify = require("spotify");
+var fs = require("fs");
 var request = require('request');
-var fs = require('fs');
-var setTask = process.argv[2];
+
+var params = process.argv.slice(2);
+
+
+switch(params[0]) {
+  case "my-tweets":
+    myTweets();
+    break;
+  
+}
+
+function myTweets(){
+  var client = new Twitter(keys.twitterKeys);
+    
+  client.get('statuses/user_timeline', {screen_name: 'PherPher_'}, function (error, data, response){
+    if(error) throw error;
+    for(var i = 0; i < data.length; i++){
+      
+      var tweetResults = data[i].text + "\n";
+      
+      console.log(tweetResults);
+      
+      };
+  });
+}  
+
+
+
+
