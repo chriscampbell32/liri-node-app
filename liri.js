d36f7fefee957e28258800c542412e40b94d3d05
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-19 14:27:49 -0500

    var client change

--- liri.js
+++ liri.js
@@ -15,7 +15,12 @@ switch(params[0]) {
 }
 
 function myTweets(){
-  var client = new Twitter(keys.twitterKeys);
+   var client = new Twitter({
+      consumer_key: keys.twitterKeys.consumer_key,
+      consumer_secret: keys.twitterKeys.consumer_secret,
+      access_token_key: keys.twitterKeys.access_token_key,
+      access_token_secret: keys.twitterKeys.access_token_secret
+    });
     
   client.get('statuses/user_timeline', {screen_name: 'PherPher_'}, function (error, data, response){
     if(error) throw error;
